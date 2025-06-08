773c1afe5fcb4452fe4ffd93a386c5a4ddfbddb4
Athanase <athanasekouassi@hotmail.com>
2025-06-08 23:57:49 +0200

    creer la structure membre

---
+++ src/app/features/membres/service.ts
@@ -0,0 +1,20 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Member} from '../models/members';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Service {
+
+  private http = inject(HttpClient);
+
+  getAll() {
+    return this.http.get<Member[]>('/api/members');
+  }
+
+  create(){
+
+  }
+
+}
---
+++ src/app/features/models/members.ts
@@ -0,0 +1,36 @@
+
+export interface Member {
+  firstName: string;
+  lastName: string;
+  email: string;
+  birthDate: Date | null;
+  gender: string;
+  phone: string;
+  address: string;
+  idNumber: string;
+  entryDate: Date | null;
+  maritalStatus: string;
+  weddingDate: Date | null;
+  weddingOfficiant: string;
+  profession: string;
+  baptismDate: Date | null;
+  baptismOfficiant: string;
+  childNumber: number;
+  originChurch: string;
+  consentToSharePersonalInfo: boolean;
+  consentToUseImageInVisuals: boolean;
+  idGroup: number | null;
+  idResponsability: number | null;
+}
+
+export enum Gender {
+  MALE = 'HOMME',
+  FEMALE = 'FEMME',
+}
+
+export enum MaritalStatus {
+  SINGLE = 'CÉLIBATAIRE',
+  MARRIED = 'MARIÉ(E)',
+  DIVORCED = 'DIVORCÉ(E)',
+  WIDOWED = 'VEUF / VEUVE',
+}
