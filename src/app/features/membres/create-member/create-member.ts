2e433955e3936d571d5c0ecb1ca6ac027a28ca9c
Athanase <athanasekouassi@hotmail.com>
2025-06-12 15:48:01 +0200

    Generate component CreateMember

---
+++ src/app/features/membres/create-member/create-member.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-create-member',
+  imports: [],
+  standalone:true,
+  templateUrl: './create-member.html',
+  styleUrl: './create-member.scss'
+})
+export class CreateMember {
+
+}
