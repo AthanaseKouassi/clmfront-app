38d89b2c612e37e4272da1d48d98b5258aadff2a
Athanase <athanasekouassi@hotmail.com>
2025-06-13 19:38:31 +0200

    generate component membre details

---
+++ src/app/features/membres/details/details.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-details',
+  imports: [],
+  standalone:true,
+  templateUrl: './details.html',
+  styleUrl: './details.scss'
+})
+export class Details {
+
+}
