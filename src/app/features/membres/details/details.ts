2395263e6fbac2ebc288d3195793647811f83967
Athanase <athanasekouassi@hotmail.com>
2025-06-14 18:33:36 +0200

    Afficher la page détail

--- src/app/features/membres/details/details.ts
+++ src/app/features/membres/details/details.ts
@@ -1,12 +1,64 @@
-import {Component} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from '@angular/material/card';
+import {MatCheckbox} from '@angular/material/checkbox';
+import {MatFormField, MatInput, MatLabel} from '@angular/material/input';
+import {ReactiveFormsModule} from '@angular/forms';
+import {Member} from '../../models/members';
+import {DatePipe} from '@angular/common';
+import {MemberService} from '../member-service';
 
 @Component({
   selector: 'app-details',
-  imports: [],
+  imports: [
+    MatFormField,
+    MatInput,
+    MatLabel,
+    MatFormField,
+    MatCheckbox,
+    ReactiveFormsModule,
+    MatCardContent,
+    MatCardTitle,
+    MatCard,
+    DatePipe,
+    MatCardHeader
+  ],
   standalone:true,
   templateUrl: './details.html',
   styleUrl: './details.scss'
 })
-export class Details {
+export class Details implements OnInit{
+
+  private readonly membreService = inject(MemberService);
+
+  member: Member = {
+    id: 0,
+    lastName: '',
+    firstName: '',
+    email: '',
+    birthDate: null,
+    gender: '',
+    address: '',
+    phone: '',
+    idNumber: '',
+    baptismDate: null,
+    baptismOfficiant: '',
+    entryDate: null,
+    maritalStatus: '',
+    weddingDate: null,
+    weddingOfficiant: '',
+    childNumber: 0,
+    originChurch: '',
+    profession: '',
+    idResponsability: null,
+    idGroup: null,
+    consentToSharePersonalInfo: false,
+    consentToUseImageInVisuals: false
+  };
+
+  ngOnInit(): void {
+  }
+
+
+
 
 }
--- src/app/features/membres/routes.ts
+++ src/app/features/membres/routes.ts
@@ -1,9 +1,8 @@
 import {Routes} from '@angular/router';
 import {List} from './list/list';
-import {MemberForm} from './member-form/member-form';
+import {Details} from './details/details';
 
 export const membresRoutes: Routes = [
   { path: '', component: List },
-  { path: 'new', component: MemberForm },
-  { path: ':id/edit', component: MemberForm }
+  { path: 'details/:id', component: Details }
 ];
